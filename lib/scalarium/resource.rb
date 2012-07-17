ecbfb2f7bec5bc76539537307889fc3f2f57f9fe
till <till@php.net>
2012-07-17 19:57:54 +0200

    Make OpenStruct behave on Ruby < 1.9 (h/t @rmehner)!

--- lib/scalarium/resource.rb
+++ lib/scalarium/resource.rb
@@ -1,3 +1,8 @@
+if RUBY_VERSION < '1.9'
+  # monkey patching open struct to allow overwriting id method
+  OpenStruct.__send__(:define_method, :id) { @table[:id] }
+end
+
 class Scalarium
   class Resource < OpenStruct
     include Scalarium::Api
