c214b5ebb6635627e8ce63220e71a7aaa00a2b4d
till <till@php.net>
2012-07-17 22:19:21 +0200

    Remove monkey patch, add id method.

--- lib/scalarium/resource.rb
+++ lib/scalarium/resource.rb
@@ -1,14 +1,13 @@
-if RUBY_VERSION < '1.9'
-  # monkey patching open struct to allow overwriting id method
-  OpenStruct.__send__(:define_method, :id) { @table[:id] }
-end
-
 class Scalarium
   class Resource < OpenStruct
     include Scalarium::Api
     def initialize(token, attributes = {})
       @token = token
       super(attributes)
+    end
+
+    def id
+      @table[:id]
     end
   end
 end
